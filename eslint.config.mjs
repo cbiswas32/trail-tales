68d4f2a935c238038b77e5d73a51354f16c4a22e
Chinmoy <chinmoy.biswas@bandhan.org>
2026-08-21 10:31:12 +0530

    Refactor configuration files: update PostCSS config, remove Tailwind config, and adjust TypeScript settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
@@ -15,21 +14,5 @@ const eslintConfig = defineConfig([
     "next-env.d.ts",
   ]),
 ]);
-=======
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
->>>>>>> a2f10a2b6e6af07e6cb2383077df18657a868919
 
 export default eslintConfig;
